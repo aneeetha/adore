b5a738c3d3b937e906604f985ba334a6dc953bc9
Anitha Kannan <anithakannan13oct@gmail.com>
2021-12-13 17:02:04 +0530

    with icon

--- app/src/main/java/com/example/adore/util/Constants.kt
+++ app/src/main/java/com/example/adore/util/Constants.kt
@@ -1,7 +1,7 @@
 package com.example.adore.util
 class Constants {
     companion object{
-        const val BASE_URL = "https://ecb5-49-204-135-1.ngrok.io/"//"https://still-sierra-33802.herokuapp.com/"
+        const val BASE_URL = "https://still-sierra-33802.herokuapp.com/"
         const val SEARCH_TIME_DELAY = 1000L
         const val CURRENCY = "Rs."
     }
